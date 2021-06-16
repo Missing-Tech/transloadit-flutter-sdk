8eb9db138713d9c422cad2b6644d1512d8e865aa
MSSNG <josephgrabski31@gmail.com>
2021-06-16 13:40:38 +0100

    Working tests

--- test/transloadit_test.dart
+++ test/transloadit_test.dart
@@ -4,5 +4,7 @@ import 'package:transloadit/transloadit.dart';
 void main() {
   test('test', () {
     expect(calculate(4, 6), 10);
+    expect(calculate(2, 4), 6);
+    expect(calculate(1, -4), -3);
   });
 }
