9d55d95541b7f6448a8b424506b1b6b659559305
MSSNG <josephgrabski31@gmail.com>
2021-06-24 16:06:44 +0100

    Template tests

--- test/transloadit_test.dart
+++ test/transloadit_test.dart
@@ -129,20 +129,78 @@ void main() {
       expect(tlResponse.data["ok"], "ASSEMBLY_COMPLETED");
       expect(tlResponse.data["template_id"], "abcdef12345");
     });
+
+    test('template', () async {
+      TransloaditTemplate template =
+          transloaditClient.newTemplate(name: "Test");
+      nock.post(startsWith("/templates"))
+        ..reply(
+          200,
+          '{"ok": "TEMPLATE_CREATED"}',
+        );
+      TransloaditResponse tlResponse = await template.createTemplate();
+      expect(tlResponse.statusCode, 200);
+      expect(tlResponse.data["ok"], "TEMPLATE_CREATED");
+    });
   });
 
-  group('cancel', () {
+  group('delete', () {
     test('assembly', () async {
       var id = "abcdef12345";
 
-      nock.delete(startsWith("/assemblies/$id"))
-        ..reply(200, '{}')
-        ..persist();
+      nock.delete(startsWith("/assemblies/$id"))..reply(200, '{}');
 
       TransloaditResponse tlResponse =
           await transloaditClient.cancelAssembly(assemblyID: id);
 
       expect(tlResponse.statusCode, 200);
+    });
+
+    test('template', () async {
+      var id = "abcdef12345";
+
+      nock.delete(startsWith("/templates/$id"))
+        ..reply(200, '{"ok": "TEMPLATE_DELETED"}');
+
+      TransloaditResponse tlResponse =
+          await transloaditClient.deleteTemplate(templateID: id);
+
+      expect(tlResponse.statusCode, 200);
+      expect(tlResponse.data["ok"], "TEMPLATE_DELETED");
+    });
+  });
+
+  group('options', () {
+    TransloaditOptions options = TransloaditOptions();
+    options.addStep("import", "/http/import", {
+      "url": "https://demos.transloadit.com/inputs/chameleon.jpg",
+    });
+    options.addStep("resize", "/image/resize", {
+      "use": "import",
+      "height": 400,
+    });
+
+    test('add step', () {
+      expect(options.steps, {
+        'import': {
+          'url': 'https://demos.transloadit.com/inputs/chameleon.jpg',
+          'robot': '/http/import'
+        },
+        'resize': {'use': 'import', 'height': 400, 'robot': '/image/resize'}
+      });
+    });
+
+    test('remove step', () {
+      options.removeStep("import");
+      expect(options.steps.containsKey("import"), false);
+    });
+
+    test('get', () {
+      expect(options.options, {
+        "steps": {
+          "resize": {"robot": "/image/resize", "use": "import", "height": 400}
+        }
+      });
     });
   });
 }
